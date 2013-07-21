928ffeeb24975f4728cac2ec47b7cad14c3baa80
Thomas Brennetot <thomas.brennetot@gmail.com>
2013-07-21 16:54:27 +0200

    more responses

--- responses.hpp
+++ responses.hpp
@@ -5,6 +5,11 @@
 
 using boost::asio::ip::tcp;
 
+void	response200(tcp::socket& socket);
+void	response201(tcp::socket& socket);
+void	response204(tcp::socket& socket);
+void	response400(tcp::socket& socket);
+void	response404(tcp::socket& socket);
 void	response501(tcp::socket& socket);
 
 #endif	// !__ERRORS__
