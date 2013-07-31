08c3a64921cfde37ff7a249aaac1451011f067b6
Thomas Brennetot <thomas.brennetot@gmail.com>
2013-08-01 01:16:16 +0200

    clean and build solution

---
+++ myrestsql2/build.bat
@@ -0,0 +1,9 @@
+:clean solution
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" myrestsql2.sln /Clean
+
+:build solution
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe myrestsql2.sln
+:/property:Configuration=Release
+
+:copy exe
+copy Debug\myrestsql2.exe myrestsql.exe
